c8c6a6b0becb898d64b9498e6350bc2cb78285cc
holypetrus1 <petrus@petrusserver.de>
2026-07-10 10:50:30 +0200

    Bump offline cache to v0.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='urlaub26-v08';
+const CACHE='urlaub26-v09';
 const CORE=['./','./index.html','./manifest.json','./icon.svg','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const req=e.request;if(req.method!=='GET')return;e.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{const copy=res.clone();try{const u=new URL(req.url);if(req.url.includes('tile.openstreetmap.org')||req.url.includes('unpkg.com')||req.url.includes('images.unsplash.com')||u.origin===location.origin){caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{})}}catch(e){}return res}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{const req=e.request;if(req.method!=='GET')return;e.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{const copy=res.clone();try{const u=new URL(req.url);if(req.url.includes('tile.openstreetmap.org')||req.url.includes('unpkg.com')||u.origin===location.origin){caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{})}}catch(e){}return res}).catch(()=>caches.match('./index.html'))))});
