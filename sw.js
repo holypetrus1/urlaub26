21e2603ecc65302f9770a9458946aa5d21b9799f
holypetrus1 <petrus@petrusserver.de>
2026-07-10 15:49:17 +0200

    Cache direct Supabase REST client

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='urlaub26-v12';
-const CORE=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js','https://unpkg.com/@supabase/supabase-js@2'];
+const CACHE='urlaub26-v13';
+const CORE=['./','./index.html','./style.css','./app.js','./db.js','./manifest.json','./icon.svg','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./index.html'))))});
